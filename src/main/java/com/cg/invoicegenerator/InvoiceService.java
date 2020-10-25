a09b64b60c8407c8fea7138b7cfadef575997cab
ibalram <balramsinghrathore3@gmail.com>
2020-10-26 02:10:21 +0530

    Added: Ability to calculate total fare of multiple rides

--- src/main/java/com/cg/invoicegenerator/InvoiceService.java
+++ src/main/java/com/cg/invoicegenerator/InvoiceService.java
@@ -1,5 +1,7 @@
 package com.cg.invoicegenerator;
 
+import com.cg.invoicegenerator.model.Ride;
+
 public class InvoiceService {
 	private static final int MINIMUM_COST_PER_KM = 10;
 	private static final int COST_PER_TIME = 1;
@@ -8,6 +10,13 @@ public class InvoiceService {
 	public double calculateFare(double distance, int time) {
 		double totalFare = distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
 		return Math.max(MINIMUM_FARE, totalFare);
+	}
+
+	public double calculateFare(Ride[] rides) {
+		double totalFare = 0;
+		for (Ride ride : rides)
+			totalFare += this.calculateFare(ride.getDistance(), ride.getTime());
+		return totalFare;
 	}
 
 }
--- src/test/java/com/cg/invoicegenerator/AppTest.java
+++ src/test/java/com/cg/invoicegenerator/AppTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.cg.invoicegenerator.model.Ride;
+
 public class AppTest {
 	private InvoiceService invoiceService;
 
@@ -20,12 +22,19 @@ public class AppTest {
 		double fare = invoiceService.calculateFare(distance, time);
 		assertEquals(25, fare, 0.0);
 	}
-	
+
 	@Test
 	public void givenLessDistanceOrTime_ShouldReturnMinFare() {
 		double distance = 0.1;
 		int time = 1;
 		double fare = invoiceService.calculateFare(distance, time);
 		assertEquals(5, fare, 0.0);
+	}
+
+	@Test
+	public void givenMultipleRides_ShouldReturnInvoiceSummary() {
+		Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
+		double totalFare = invoiceService.calculateFare(rides);
+		assertEquals(30.0, totalFare, 0.0);
 	}
 }
